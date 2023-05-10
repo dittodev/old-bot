d8ac9eb4e68fb840a03e9dc22f32e2c5d4add3f3
Gurkan <hello@gurkz.me>
2023-05-10 18:52:44 +0200

    sharding

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,10 @@
-import 'dotenv/config'
 import { IntentsBitField } from 'discord.js'
 import { ExtendedClient } from './client/Client';
 import { env } from './env';
 
 export const client = new ExtendedClient(
     {
-        intents: IntentsBitField.Flags.Guilds
+        intents: IntentsBitField.Flags.Guilds,
     }
 );
 
---
+++ src/launcher.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import { ShardingManager } from "discord.js";
+import { env } from "./env";
+
+const manager = new ShardingManager(__dirname + '/bot.ts', { token: env.TOKEN, execArgv: ['-r', 'ts-node/register'] });
+
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+
+manager.spawn();
