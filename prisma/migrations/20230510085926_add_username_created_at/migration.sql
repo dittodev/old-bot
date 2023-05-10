50c980fe2c389df483d4f939fee87d531c1380ef
Gurkan <hello@gurkz.me>
2023-05-10 11:37:18 +0200

    create a new `user` on interactionCreate

---
+++ prisma/migrations/20230510085926_add_username_created_at/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `createdAt` DATETIME(3) NULL,
+    ADD COLUMN `username` VARCHAR(191) NULL;
--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,7 +1,34 @@
-import { CommandInteractionOptionResolver } from "discord.js";
+import { CommandInteractionOptionResolver, User as DUser } from "discord.js";
 import { client } from "../bot";
 import { Event } from "../structures/Event";
 import { Command, ExtendedInteraction } from "../types/Command";
+import prisma from "../prisma";
+
+async function MigratePrismaUser(user: DUser) {
+    const prismaUser = await prisma.user.findUnique({
+        where: {
+            discord_id: user.id
+        }
+    });
+
+    if (!prismaUser) {
+        user.send(`Hello, ${user.username}! Welcome to ${client.user?.username} bot!`)
+        const freshUser = await prisma.user.create({
+            data: {
+                discord_id: user.id,
+                createdAt: user.createdAt,
+                username: user.username
+            }
+        });
+
+        return freshUser;
+    }
+
+    if (prismaUser.username !== user.username) await prisma.user.update({ where: { discord_id: user.id }, data: { username: user.username } })
+    if (prismaUser.createdAt !== user.createdAt) await prisma.user.update({ where: { discord_id: user.id }, data: { createdAt: user.createdAt } })
+
+    return prismaUser
+}
 
 export default new Event("interactionCreate", async (interaction) => {
     // Chat Input Commands
@@ -9,7 +36,9 @@ export default new Event("interactionCreate", async (interaction) => {
         await interaction.deferReply();
         const command: Command | undefined = client.commands.get(interaction.commandName);
         if (!command)
-            return interaction.followUp("You have used a non exitent command");
+            return interaction.followUp("You have used a non existent command");
+
+        const prismaUser = await MigratePrismaUser(interaction.user);
 
         command.callback({
             args: interaction.options as CommandInteractionOptionResolver,
