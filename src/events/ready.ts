37e3e2d351564dfb1ad7ab00d273ca058e005c54
Gurkan <hello@gurkz.me>
2023-05-11 10:39:26 +0200

    add status about which shard

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,11 @@
+import { ActivityType } from "discord.js";
+import { client } from "../bot";
 import { Event } from "../structures/Event";
 
 export default new Event("ready", () => {
-    console.log("Bot is online");
+    console.log(`shard ${client.shard?.ids} is online`);
+    client.user?.setActivity({
+        name: `shard ${client.shard?.ids}`,
+        type: ActivityType.Listening
+    })
 });
--- src/launcher.ts
+++ src/launcher.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { ShardingManager } from "discord.js";
 import { env } from "./env";
 
-const manager = new ShardingManager(__dirname + '/bot.ts', { token: env.TOKEN, execArgv: ['-r', 'ts-node/register'] });
+const manager = new ShardingManager(__dirname + '/bot.ts', { token: env.TOKEN, execArgv: ['-r', 'ts-node/register'], totalShards: "auto" });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
